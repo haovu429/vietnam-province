56eb106e3d6a2ae54d10ba222dc481e2bbe00ab4
HaoVu <caihoncuagiamnguc@gmail.com>
2023-08-14 16:32:43 +0700

    first commit

---
+++ src/main/java/ute/haovu/vietnam_province/VietnamProvinceApplication.java
@@ -0,0 +1,13 @@
+package ute.haovu.vietnam_province;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VietnamProvinceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VietnamProvinceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ute/haovu/vietnam_province/VietnamProvinceApplicationTests.java
@@ -0,0 +1,13 @@
+package ute.haovu.vietnam_province;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VietnamProvinceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
